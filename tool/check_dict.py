420c8b731b6177ccc695a9990ed1d4ca80880d27
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-06-25 16:52:35 +0700

    initial

--- tool/check_dict.py
+++ tool/check_dict.py
@@ -3,9 +3,9 @@ from dataset.alphabet import Alphabet
 alphabet = Alphabet(r'D:\workspace\project\pren\asset\viet_alphabet.txt', 128)
 dict_path = r'D:\workspace\project\pren\tool\line_dict.txt'
 with open(dict_path, 'r', encoding='utf-8') as f:
-    line_dict = [alphabet.decode(alphabet.encode(line.strip())) for line in f.readlines()]
+    line_dict = {alphabet.decode(alphabet.encode(line.strip())): 1 for line in f.readlines()}
 
 with open(r"D:\workspace\project\pren\tool\checked_dict.txt", 'w', encoding='utf-8') as f:
-    for item in line_dict:
+    for item, _ in line_dict.items():
         f.write(item)
         f.write("\n")
--- tool/data_splitting.py
+++ tool/data_splitting.py
@@ -5,20 +5,22 @@ import random
 dict_path = r'D:\workspace\project\pren\tool\checked_dict.txt'
 with open(dict_path, 'r', encoding='utf-8') as f:
     line_dict = [line.strip() for line in f.readlines()]
+
+print(len(line_dict))
 random.shuffle(line_dict)
 save_path = r'D:\workspace\project\pren\tool\data'
-valid_len = 100000
+valid_len = 64000
 valid = line_dict[:valid_len]
 with open(os.path.join(save_path, 'valid.txt'), 'w', encoding='utf-8') as f:
     for item in valid:
         f.write(item)
         f.write("\n")
-test = line_dict[valid_len:valid_len + 100000]
+test = line_dict[valid_len:valid_len + 64000]
 with open(os.path.join(save_path, 'test.txt'), 'w', encoding='utf-8') as f:
     for item in test:
         f.write(item)
         f.write("\n")
-train = line_dict[valid_len + 100000:]
+train = line_dict[valid_len + 64000:]
 with open(os.path.join(save_path, 'train.txt'), 'w', encoding='utf-8') as f:
     for item in train:
         f.write(item)
