e533f460908e2cdabdb1f880e8c6adbfd1966a2e
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-06-26 17:26:04 +0700

    initial

--- criterion/attn_loss.py
+++ criterion/attn_loss.py
@@ -1,12 +1,10 @@
 from torch import nn, Tensor
-from typing import Tuple
 
 
 class AttnLoss(nn.Module):
     def __init__(self, pad: int):
         super().__init__()
         self._criterion: nn.Module = nn.CrossEntropyLoss(ignore_index=pad)
-        self._pad: int = pad
 
     def forward(self, pred: Tensor, target: Tensor) -> Tensor:
         target = target.contiguous().view(-1)
